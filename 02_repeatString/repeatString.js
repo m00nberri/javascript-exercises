1ec70abd783b681b7ff744df4fc52ceb45b39f5b
m00nberri <zaberkat@gmail.com>
2022-05-18 12:33:00 +0900

    Completed Task 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    if (num < 0) {
+        return `ERROR`
+    }
 
+    let text = ``
+    for (let i = 1; i<=num; i++) {
+        text += string;
+    }
+    
+    return text;
 };
 
 // Do not edit below this line
