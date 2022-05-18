e3afc7eed3cbeb5a8d4845d469942508710459b7
m00nberri <zaberkat@gmail.com>
2022-05-18 12:57:47 +0900

    Completed Task 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = string.slice(-1)
+    for (let i = 1; i <= string.length; i++) {
+        reversedString += string.slice(-i,-(i-1));
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
