b51fb9a3c7507a4664064a4b32f332361ddb9713
m00nberri <zaberkat@gmail.com>
2022-05-22 10:49:23 +0900

    Completed Task 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...trash) {
+    return array.filter(value => !trash.includes(value))
 };
 
 // Do not edit below this line
