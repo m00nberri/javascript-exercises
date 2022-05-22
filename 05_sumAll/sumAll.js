fd8b5bd988189eb8efdd15c8cc78d0c8cd87ad35
m00nberri <zaberkat@gmail.com>
2022-05-22 11:35:31 +0900

    Completed Task 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(...minMax) {
+    let sum = 0;
+    let min = Math.min(...minMax);
+    let max = Math.max(...minMax);
+    if ((min >= 0) && minMax.every(value => typeof(value) === 'number')) {
+        for (i = min; i <= max; i++) {
+            sum += i
+        };
+        return sum
+    }
+    else {
+        return 'ERROR'
+    };
 
 };
 
